fae6f7753ff34f42b2822906d2a8ddd0997cb873
Adeoluwa Opadina <atopadina@gmail.com>
2026-01-20 09:08:16 -0500

    Fix frontend API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
-const config = {
-  backendUrl: "http://<ALB-DNS>/api"
-};
-export default config;
+const API_URL = "http://devops-challenge-alb-312401820.us-east-1.elb.amazonaws.com/api";
+export default API_URL;
